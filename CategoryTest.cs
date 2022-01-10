f4299d9ed2457929f5271be18462705425bc4372
José Victor <95879558+JVictorSegundo@users.noreply.github.com>
2022-01-09 23:29:52 -0300

    Add files via upload

---
+++ CategoryTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using LeagueStore.Domain.Entities;
+using System;
+using Xunit;
+using LeagueStore.Domain.ValueObjects;
+
+namespace LeagueStore.Domain.Tests
+{
+    public class CategoryTest
+    {
+        [Fact]
+        public void CreateCategory_WithValideParameters_ResultValidState()
+        {
+            Action action = () => new Category("Defesa");
+            action.Should().NotThrow<LeagueStore.Domain.Validation.DomainValidation>();
+        }
+        //---
+
+        [Fact]
+        public void CreateCategory_WithBlankParameters_ResultInvalidState()
+        {
+            Action action = () => new Category("    ");
+            action.Should().Throw<LeagueStore.Domain.Validation.DomainValidation>();
+        }
+        //---
+
+        [Fact]
+        public void CreateCategory_WithTooLongParameters_ResultInvalidState()
+        {
+            Action action = () => new Category("alsdkaoskdpaskdpajskdjasodjasldkjalskdjaskjdlaskjdlaksjdlkasjdlkasj");
+            action.Should().Throw<LeagueStore.Domain.Validation.DomainValidation>();
+        }
+        //---
+
+        [Fact]
+        public void CreateCategory_WithSpecialParameters_ResultInvalidState()
+        {
+            Action action = () => new Category ("@sd");
+            action.Should().Throw<LeagueStore.Domain.Validation.DomainValidation>();
+        }
+    }
+}
+
