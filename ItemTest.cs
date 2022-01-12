43a547d5a987c18d7b75ae77655a6e04e8429f58
José Victor <95879558+JVictorSegundo@users.noreply.github.com>
2022-01-12 15:38:29 -0300

    Add files via upload

--- ItemTest.cs
+++ ItemTest.cs
@@ -1,8 +1,9 @@
 using FluentAssertions;
-using LeagueStore.Domain.Entities;
+using I_LeagueStore.Domain.Entities;
 using System;
 using Xunit;
-using LeagueStore.Domain.ValueObjects;
+using I_LeagueStore.Domain.ValueObjects;
+using I_LeagueStore.Domain.Validation;
 
 namespace LeagueStore.Domain.Tests
 {
@@ -11,57 +12,58 @@ namespace LeagueStore.Domain.Tests
         [Fact]
         public void CreateItem_WithValideParameters_ResultValidState()
         {
-            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", new Attack("2", "30"), new Defense("10", "1", "13"));
-            action.Should().NotThrow<LeagueStore.Domain.Validation.DomainValidation>();
+            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", "120", new Attack("2", "30"), new Defense("10", "1", "13"));
+            action.Should().NotThrow<DomainValidation>();
         }
         //---
 
         [Fact]
         public void CreateItem_WithSpecialParameters_ResultInvalidState()
         {
-            Action action = () => new Item("Ek@-de_Luden", "Um item poderoso que libera=você_das amarras do destino", new Attack("2", "30"), new Defense("10", "1", "13"));
-            action.Should().Throw<LeagueStore.Domain.Validation.DomainValidation>();
+            Action action = () => new Item("Ek@-de_Luden", "Um item poderoso que libera=você_das amarras do destino", "120", new Attack("2", "30"), new Defense("10", "1", "13"));
+            action.Should().Throw<DomainValidation>();
         }
         //---
 
         [Fact]
         public void CreateItem_WithEmptyParameters_ResultInvalidState()
         {
-            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", new Attack("0", ""), new Defense("0", "0", "0"));
-            action.Should().Throw<LeagueStore.Domain.Validation.DomainValidation>();
+            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", "120", new Attack("0", ""), new Defense("0", "0", "0"));
+            action.Should().Throw<DomainValidation>();
         }
         //---
 
         [Fact]
         public void CreateItem_WithNullParameters_ResultInvalidState()
         {
-            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", new Attack("0", null), new Defense("0", "0", "0"));
-            action.Should().Throw<LeagueStore.Domain.Validation.DomainValidation>();
+            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", "120", new Attack("0", null), new Defense("0", "0", "0"));
+            action.Should().Throw<DomainValidation>();
         }
         //---
         
         [Fact]
         public void CreateItem_WithBellowZeroParameters_ResultInvalidState()
         {
-            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", new Attack("-30", "0"), new Defense("0", "0", "0"));
-            action.Should().Throw<LeagueStore.Domain.Validation.DomainValidation>();
+            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", "120", new Attack("-30", "0"), new Defense("0", "0", "0"));
+            action.Should().Throw<DomainValidation>();
         }
         //---
 
         [Fact]
         public void CreateItem_WithAboveLimitsParameters_ResultInvalidState()
         {
-            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", new Attack("180", "00"), new Defense("0", "0", "0"));
-            action.Should().Throw<LeagueStore.Domain.Validation.DomainValidation>();
+            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", "120", new Attack("180", "00"), new Defense("0", "0", "0"));
+            action.Should().Throw<DomainValidation>();
         }
         //---
 
         [Fact]
         public void CreateItem_WithNoConvertibleParameters_ResultInvalidState()
         {
-            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", new Attack("adsdasd", "00"), new Defense("0", "0", "0"));
-            action.Should().Throw<LeagueStore.Domain.Validation.DomainValidation>();
+            Action action = () => new Item("Eko de Luden", "Um item poderoso que libera você das amarras do destino", "120", new Attack("adsdasd", "00"), new Defense("0", "0", "0"));
+            action.Should().Throw<DomainValidation>();
         }
+        //---
 
     }
 }
